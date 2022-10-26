399e3e93bf265b5f72a15b45ebabe68ccb97db3b
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:34:42 -0400

    Add files via upload

---
+++ Membres_statiques/Membres_statiques/Classe_statique/Membres_statiques.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_statique
+{
+    class Membres_statiques
+    {
+        
+        public int numero; // Variable d'instance.
+        public static int nb_instances=0; // Variable de classe ou variable statique.
+        //Méthode statique
+        public static void Afficher()
+        {
+            Console.WriteLine("\nJe suis une méthode statique.");
+        }
+        public static void Incrementer()
+        {
+            nb_instances++;
+            Console.WriteLine("Le nombre d'objets instanciés est égal à "+ nb_instances);
+        }
+    }
+}
---
+++ Membres_statiques/Membres_statiques/Classe_statique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Classe_statique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancier un premier objet
+            // et initialiser la variable d'instance numero
+            Membres_statiques c_stat1 = new Membres_statiques();
+            c_stat1.numero = 10;
+            // Incrémenter la variable de classe nb_insances
+            Membres_statiques.nb_instances += 1;
+            // Instancier un deuxième objet
+            // et initialiser la variable d'instance numero
+            Membres_statiques c_stat2 = new Membres_statiques();
+            c_stat2.numero = 20;
+            // Incrémenter la variable de classe nb_insances
+            Membres_statiques.nb_instances += 1;
+            Console.Write("L'attribut statique nb_instances est égal à : "+Membres_statiques.nb_instances);
+            Membres_statiques.Afficher();
+            // Instancier un nouvel objet puis appeler la méthode statique Incrementer()
+            Membres_statiques c_stat3 = new Membres_statiques();
+            Membres_statiques.Incrementer();
+            Console.ReadKey();
+            
+        }
+    }
+}
